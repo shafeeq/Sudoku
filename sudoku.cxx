789c9708ba1193d279a6fc018c6952a374cc23a1
Shafeeq K <shafeeq94@gmail.com>
2011-08-15 08:03:15 +0530

    Fixed indendations

--- sudoku.cxx
+++ sudoku.cxx
@@ -28,99 +28,83 @@ int sudoku[ORDER][ORDER];
 int Check (int y[]);
 int temp[ORDER];
 
-int RowCheck ( int x[ORDER][ORDER]) 
-{
-	int i;
-	int l;
+int RowCheck ( int x[ORDER][ORDER]) {
+  int i;
+  int l;
 	
-	for (i=0; i<ORDER; i++)
-	{
-		for (l= 0; l<ORDER;l++)
-			temp[l] = x[i][l];
+  for (i=0; i<ORDER; i++) {
+    for (l= 0; l<ORDER;l++)
+      temp[l] = x[i][l];
 			
-		if ( Check (temp) == 0 )
-			return 0;
-	}
-	return 1;
+    if ( Check (temp) == 0 )
+      return 0;
+  }
+  return 1;
 }
 									
-int Check (int y[ORDER])
-{
-	int lineartemp;
-	for (int i=0; i<ORDER; i++)
-	{
-		lineartemp = y[i];
-		for (int j=i+1; j<ORDER; j++)
-		{
-			if (y[j] ==  lineartemp)
-				return 0;
-		}
-	}
-	return 1;
+int Check (int y[ORDER]) {
+  int lineartemp;
+  for (int i=0; i<ORDER; i++) {
+    lineartemp = y[i];
+    for (int j=i+1; j<ORDER; j++) {
+      if (y[j] ==  lineartemp)
+	return 0
+	  }
+  }
+  return 1;
 }
 
-int ColumnCheck ( int x[ORDER][ORDER]) 
-{
-	int i;
-	int l;
-	for (i=0; i<ORDER; i++)
-	{
-		for (l= 0; l<ORDER;l++)
-			temp[l] = x[l][i];
+int ColumnCheck ( int x[ORDER][ORDER]) {
+  int i;
+  int l;
+  for (i=0; i<ORDER; i++) {
+    for (l= 0; l<ORDER;l++)
+      temp[l] = x[l][i];
 		
-			if ( Check (temp) == 0 )
-				return 0;
-	}
-	return 1;
+    if ( Check (temp) == 0 )
+      return 0;
+  }
+  return 1;
 }			
 
-int SquaresCheck (int x[ORDER][ORDER])
-{
-	int i;
-	int j;
-	int column;
-	int row;
-	int k;
+int SquaresCheck (int x[ORDER][ORDER]) {
+  int i;
+  int j;
+  int column;
+  int row;
+  int k;
 	
-	for (column=0; column <= 6; column += 3)
-	{
-		for (row=0; row <= 6; row += 3)
-		{
-			for (k=0, i=0; i<3; i++)
-			{
-				for (j=0; j<3; j++)
-					temp[k++] = x[row+i][column+j];
-			}
-			if (Check(temp) ==0)
-				return 0;
-			
-
-			
-		}
-	}
+  for (column=0; column <= 6; column += 3) {
+    for (row=0; row <= 6; row += 3) {
+      for (k=0, i=0; i<3; i++) {
+	for (j=0; j<3; j++)
+	  temp[k++] = x[row+i][column+j];
+      }
+      if (Check(temp) ==0)
+	return 0;		
+    }
+  }
 	
-	return 1;
+  return 1;
 }
 			
+
 	
-int main()
-{
-	int i;
-	int j;
+int main() {
+  int i;
+  int j;
 	
-	for ( i=0; i<ORDER; i++)
-	{
-		for ( j=0; j<ORDER; j++)
-			cin >> sudoku[i][j];
-	}
+  for ( i=0; i<ORDER; i++) {
+    for ( j=0; j<ORDER; j++)
+      cin >> sudoku[i][j];
+  }
 	
-	if ( RowCheck(sudoku) == 0 || ColumnCheck(sudoku)==0 || SquaresCheck(sudoku)==0)
-	{
-		cout<<"\nSudoku is incorrect\n";
-		return 0;
-	}
+  if ( RowCheck(sudoku) == 0 || ColumnCheck(sudoku)==0 || SquaresCheck(sudoku)==0) {
+    cout<<"\nSudoku is incorrect\n";
+    return 0;
+  }
 	
-	cout<<"\nSudoku is correct\n";
-	return 0;
+  cout<<"\nSudoku is correct\n";
+  return 0;
 }
 
